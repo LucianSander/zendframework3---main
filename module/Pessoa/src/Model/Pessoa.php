370e5e600a23f2af607be162c85298733de94fd5
Lucian Sander <sanderluciansander2015@gmail.com>
2023-01-31 14:43:45 -0300

    Rodando sem nem um erro, terminar as aulas

--- module/Pessoa/src/Model/Pessoa.php
+++ module/Pessoa/src/Model/Pessoa.php
@@ -2,49 +2,60 @@
 
 namespace Pessoa\Model;
 
-class Pessoa {
+class Pessoa{
+
     private $id;
     private $nome;
     private $sobrenome;
     private $email;
     private $situacao;
 
-    public function exchangeArray (array $data){
-        $this->id = !emply($data['id']) ? $data['id'] : null;
-        $this->nome = !emply($data['nome']) ? $data['nome'] : null;
-        $this->sobrenome = !emply($data['sobrenome']) ? $data['sobrenome'] : null;
-        $this->email = !emply($data['email']) ? $data['email'] : null;
-        $this->situacao = !emply($data['situacao']) ? $data['situacao'] : null;
+    public function exchangeArray(array $data){
+
+        //operador ternario 
+
+        $this->id        = !empty($data['id'])        ? $data['id']          : null;
+        $this->nome      = !empty($data['nome'])      ? $data['nome']        : null;
+        
+        $this->sobrenome = !empty($data['sobrenome']) ? $data['sobrenome']   : null;
+        $this->email     = !empty($data['email'])     ? $data['email']       : null;
+        
+        $this->situacao  = !empty($data['situacao'])  ? $data['situacao']    : null;
+   
     }
 
     public function getId(){
         return $this->id;
     }
     public function setId($id){
-        $this->id =$id;
+        $this->id = $id;
     }
+
     public function getNome(){
-        return $this->Nome;
+        return $this->nome; 
     }
     public function setNome($nome){
-        $this->nome =$nome;
+        $this->nome = $nome;
     }
+
     public function getSobrenome(){
         return $this->sobrenome;
     }
     public function setSobrenome($sobrenome){
-        $this->sobrenome =$sobrenome;
+        $this->sobrenome = $sobrenome;
     }
+
     public function getEmail(){
         return $this->email;
     }
     public function setEmail($email){
-        $this->email =$email;
+        $this->email = $email;
     }
     public function getSituacao(){
         return $this->situacao;
     }
     public function setSituacao($situacao){
-        $this->situacao =$situacao;
+        $this->situacao = $situacao;
     }
+    
 }
