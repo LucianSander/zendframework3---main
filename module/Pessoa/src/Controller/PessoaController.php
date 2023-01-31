c20b33a985e132d981ff4b11317f03e4b19d3517
Lucian Sander <sanderluciansander2015@gmail.com>
2023-01-31 17:51:20 -0300

    falta instalar o plugin form e o i18n

--- module/Pessoa/src/Controller/PessoaController.php
+++ module/Pessoa/src/Controller/PessoaController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Pessoa\Controller;
+
+use Pessoa\Form\PessoaForm;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
@@ -23,7 +25,25 @@ class PessoaController extends AbstractActionController
 
 //Adicionar pessoas no modulo pessoa
     public function adicionarAction(){
-        return new ViewModel();
+        $form = new PessoaForm();
+        $form->get('submit')->setValue('Adicionar');
+        $request = $this->getRequest();
+        if (!$request->isPost()) {
+            return new ViewModel(['form'=>$form]);
+        }
+        //setData vai preencher a tabela
+        $pessoa = new \Pessoa\Model\Pessoa();
+        $form->setData($request->getPost());
+        if (!$form->isValid()) {
+            return new ViewModel(['form'=>$form]);
+        }
+        $pessoa->exchangeArray($form->getData());
+        $this->table->salvarPessoa($pessoa);
+        
+        //vai para a home
+        return $this->redirect()->toRoute('pessoa');
+
+        //return new ViewModel();
     }
 
     public function salvarAction(){
