e2e928e18fa733a0230ef23940828e44a4778c11
Lucian Sander <sanderluciansander2015@gmail.com>
2023-02-01 21:24:35 -0300

    Falta a solução do plugin

--- module/Pessoa/src/Form/PessoaForm.php
+++ module/Pessoa/src/Form/PessoaForm.php
@@ -1,22 +1,26 @@
 <?php
 
 namespace Pessoa\Form;
+
 use Zend\Form\Form;
 
-class PessoaForm extends Form {
-    public function __construct() {
+
+class PessoaForm extends Form{
+
+    public function __construct(){
+
         parent::__construct('pessoa', []);
 
         $this->add(new \Zend\Form\Element\Hidden('id'));
-        $this->add(new \Zend\Form\Element\Text("nome",['label' => "Nome"]));
-        $this->add(new \Zend\Form\Element\Text("sobrenome",['label' => "Sobrenome"]));
-        $this->add(new \Zend\Form\Element\Email("email",['label' => "Email"]));
-        $this->add(new \Zend\Form\Element\Text("situacao",['label' => "Situacao"]));
-
+        $this->add(new \Zend\Form\Element\Text("nome", ['label' => "Nome"]));
+        $this->add(new \Zend\Form\Element\Text("sobrenome", ['label' => "Sobrenome"]));
+        $this->add(new \Zend\Form\Element\Email("email", ['label' => "Email"]));
+        $this->add(new \Zend\Form\Element\Text("situacao", ['label' => "Situacao"]));
 
         $submit = new \Zend\Form\Element\Submit('submit');
         $submit->setAttributes(['value'=>'Salvar','id'=>'submitbutton']);
         $this->add($submit);
 
     }
+
 }
